69a7b89503e274d7f89ec7d633e3298898394882
Adrian Rosa <cabeza_mtz@hotmail.com>
2016-05-16 12:36:38 -0300

    Clase 17 - Callbacks
    Creamos callbacks e implementamos el visits_count

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,10 @@
 # --------------------- CONTROLADOR --------------------------
 
 class ArticlesController < ApplicationController
+	# Esto es un callback
+	before_action :authenticate_user!, only: [:create, :new]
+	before_action :set_article, except: [:index, :new, :create]
+
 
 	# GET y el path /articles
 	def index
@@ -12,11 +16,12 @@ class ArticlesController < ApplicationController
 	def show
 		# devuelve uno o varios articulos segun lo pasado en el parametro
 		# FIND devuelve el primer registro que cumpla la condicion (solo uno)
-		@article = Article.find(params[:id])
+		# ---@article = Article.find(params[:id]) REFACTORISADO EN EL CALLBACK SET_ARTICLE
 		#  WHERE devuelve varios registros que cumplan la condicion.
 		# Article.where("id = ? OR body = ?",params[:id],params[:body])
 		# where NOT devuelve todos menos el pasado por parametro
 		# Article.where.not("id = ?",params[:id])
+		@article.update_visits_count
 	end
 
 	# GET /articles/new
@@ -25,13 +30,16 @@ class ArticlesController < ApplicationController
 	end
 
 	def destroy 
-		@article = Article.find(params[:id])
 		@article.destroy # Elimina el registro de la BD.
 		redirect_to articles_path
 	end
 
 	def update
-		# @article.update_attribute({title: "Nuevo titulo"})
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render :edit
+		end
 	end
 
 	#POST /articles
@@ -45,11 +53,14 @@ class ArticlesController < ApplicationController
 	end
 
 	def edit
-		@article = Article.find(params[:id])
+		# REFACTORIZADO EN EL CALLBACK SET_ARTICLE
 	end
 
-
 	private # Todo lo que este por debajo de PRIVATE son metodos privados de la clase.
+
+	def set_article
+		@article = Article.find(params[:id])
+	end
 
 	def article_params
 		params.require(:article).permit(:title,:body)
--- app/models/article.rb
+++ app/models/article.rb
@@ -5,4 +5,16 @@ class Article < ActiveRecord::Base
 	# TIPS: las validaciones van en el modelo.
 	validates :title, presence: true, uniqueness: true
 	validates :body, presence: true, length: { minimum: 20 }
+	before_save :set_visits_count
+
+	def update_visits_count
+		self.update(visits_count: self.visits_count + 1)
+	end
+
+	private
+
+	def set_visits_count
+		self.visits_count ||= 0			
+	end
+
 end
