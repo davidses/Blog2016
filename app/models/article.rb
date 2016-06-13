f805f37e4547000c4020399d9f11ef2564eb2d20
Adrian Rosa <cabeza_mtz@hotmail.com>
2016-06-13 12:38:20 -0300

    Clase 23 - Agregamos la suba de archivos con paperclip

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,12 +1,14 @@
-# --------------------- NODELO --------------------------
-
 class Article < ActiveRecord::Base
 	belongs_to :user
 	has_many :comments
+
 	# TIPS	: las validaciones van en el modelo.
 	validates :title, presence: true, uniqueness: true
 	validates :body, presence: true, length: { minimum: 20 }
 	before_save :set_visits_count
+
+	has_attached_file :cover, styles: { } # ESTO VA EN STYLES PERO DA ERROR medium: "1280x720", thumb: "800x600"
+	validates_attachment_content_type :cover, content_type: /\Aimage\/.*\Z/
 
 	def update_visits_count
 		self.update(visits_count: self.visits_count + 1)
